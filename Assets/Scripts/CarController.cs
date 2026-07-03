f4006950a563a6cc49fd4ba24986570cd84ab94b
monolizm-pc1 <hosaka@monolizm.com>
2026-07-03 11:53:29 +0900

    試験用に改良

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     float speed = 0f;
-    Vector2 startPos;
+    public int life = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,19 +16,38 @@ public class CarController : MonoBehaviour
     {
         if ( Input.GetMouseButtonDown(0))
         {
-            this.startPos = Input.mousePosition;
-        }
-        else if ( Input.GetMouseButtonUp(0)) 
-        {
-            Vector2 endPos = Input.mousePosition;
-            float swipeLenght = endPos.x - startPos.x;
+            // マウスの座標をスクリーン座標からワールド座標に変換する
+            Vector3 mouseWorldPos = Input.mousePosition;
+            mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseWorldPos);
 
-            this.speed = swipeLenght / 2500.0f;
+            // マウスの座標と車の座標の差を計算する
+            Vector3 v = mouseWorldPos - this.transform.position;
+            float sign = Mathf.Sign(v.x);
+            this.speed = sign * 0.3f;    //0以上なら1、0未満なら-1を返す
+
+            // 車の向きを変える
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if ( sign >= 0 ) spriteRenderer.flipX = false;
+            else spriteRenderer.flipX = true;
+
+            GetComponent<AudioSource>().Play();
+
         }
 
         transform.Translate(this.speed, 0, 0);
         this.speed *= 0.98f;
     }
+
+    void OnBecameInvisible()
+    {
+        // 画面外に出たときの処理
+        this.speed *= -1;
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+
+
+    }
+
 }
 
 
--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 public class GameDirector : MonoBehaviour
 {
     GameObject car;
-    GameObject flag;
-    GameObject distance;
+    GameObject lifeText;
+
+    bool isGameOver = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         this.car = GameObject.Find("car_0");
-        this.flag = GameObject.Find("flag_0");
-        this.distance = GameObject.Find("distance");
+        this.lifeText = GameObject.Find("lifeText");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float lenght = this.flag.transform.position.x - this.car.transform.position.x;
-        if ( lenght >= 0 )
-        {
-            this.distance.GetComponent<TextMeshProUGUI>().text =
-                "Distance:" + lenght.ToString("F2") + "m";
+        CarController carController = this.car.GetComponent<CarController>();
+
+        if (carController.life > 0 )
+        { 
+            string lifeString = "";
+            for (int i = 0; i < carController.life; i++)
+            {
+                lifeString += "■";
+            }
+            this.lifeText.GetComponent<TextMeshProUGUI>().text = lifeString;
         }
-        else
-        {
-            this.distance.GetComponent<TextMeshProUGUI>().text = "GameOver";
+        else {
+            if ( !isGameOver )
+            {
+                GetComponent<AudioSource>().Play();
+                isGameOver = true;
+            }
+            this.lifeText.GetComponent<TextMeshProUGUI>().text = "GameOver";
         }
     }
 }
